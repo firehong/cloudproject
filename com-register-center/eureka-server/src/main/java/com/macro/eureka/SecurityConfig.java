0d61eb8b84ead89a64ea71ae110df5d9143db913
firehong <715292834@qq.com>
2020-06-19 17:10:02 +0800

    init

---
+++ com-register-center/eureka-server/src/main/java/com/macro/eureka/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.macro.eureka;
+
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 启用Security来控制注册中心权限
+ * spring.security.user.name/password 通过配置文件进行制定账号和密码
+ * @author LQX
+ *
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
