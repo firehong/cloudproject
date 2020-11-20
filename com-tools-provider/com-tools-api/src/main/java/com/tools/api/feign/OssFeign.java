ba2ea06d7f77a65774208bb0c7bcb493e179fce8
firehong <ll>
2020-11-20 11:29:25 +0800

    文件

--- com-tools-provider/com-tools-api/src/main/java/com/tools/api/feign/OssFeign.java
+++ com-tools-provider/com-tools-api/src/main/java/com/tools/api/feign/OssFeign.java
@@ -1,6 +1,7 @@
 package com.tools.api.feign;
 
 
+import com.tools.api.config.MultipartSupportConfig;
 import com.tools.api.feign.fallback.OssFeignClientFallbackFactory;
 import com.tools.api.vo.UploadVO;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -15,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @Date 2020/6/28 13:20
  * @Description 用户相关接口
  */
-@FeignClient(value = "ossService", fallbackFactory = OssFeignClientFallbackFactory.class)
+@FeignClient(value = "ossService", fallbackFactory = OssFeignClientFallbackFactory.class,configuration = MultipartSupportConfig.class)
 public interface OssFeign {
 
     /**
